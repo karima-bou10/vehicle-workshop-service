f2e06b8ac4604e74ac8a7ff53d524f9506a8f39b
BOUACHRA <karima.bouachra@capgemini.com>
2026-07-15 17:57:03 +0100

    entité mecanicien

--- src/main/java/com/workshop/vehicle_service/mecanicien/entity/Mecanicien.java
+++ src/main/java/com/workshop/vehicle_service/mecanicien/entity/Mecanicien.java
@@ -1,4 +1,37 @@
 package com.workshop.vehicle_service.mecanicien.entity;
 
+import com.workshop.vehicle_service.intervention.entity.Intervention;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "mecanicien")
 public class Mecanicien {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id_mecanicien")
+    private Long id;
+
+    @Column(name="nom", nullable = false)
+    private String nom;
+
+    @Column(name="prenom", nullable = false)
+    private String prenom;
+
+    @Column(name="specialite", nullable = false)
+    private String specialite;
+
+    @Column(name="disponible", nullable = false)
+    private boolean disponible = true;
+
+    @OneToMany(mappedBy = "intervention",fetch = FetchType.LAZY, cascade = {CascadeType.PERSIST, CascadeType.MERGE})
+    private List<Intervention> inetventions;
 }
