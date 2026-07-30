4c938dd710359bc97e415d4e58653372d74226a1
karima-bou10 <karima.bouachra1010@gmail.com>
2026-07-31 00:40:51 +0100

    Ajouter l'API de recherche

--- src/main/java/com/workshop/vehicle_service/mecanicien/controller/MecanicienController.java
+++ src/main/java/com/workshop/vehicle_service/mecanicien/controller/MecanicienController.java
@@ -13,8 +13,6 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.List;
-
 @RestController
 @RequestMapping("/api/mecaniciens")
 @AllArgsConstructor
@@ -63,7 +61,15 @@ public class MecanicienController {
 
     @GetMapping("/getAllDisponibles")
     @PreAuthorize("hasAnyRole('ROLE_USER','ROLE_MANAGER')")
-    public ResponseEntity<List<MecanicienResponse>> getMecaniciensDisponibles(){
-        return ResponseEntity.ok(mecanicienService.getMecaniciensDisponibles());
+    public ResponseEntity<Page<MecanicienResponse>> getMecaniciensDisponibles(@ParameterObject Pageable pageable) {
+        Page<MecanicienResponse> pageResult = mecanicienService.getMecaniciensDisponibles(pageable);
+        return ResponseEntity.ok(pageResult);
+    }
+
+    @GetMapping("/search")
+    @PreAuthorize("hasAnyRole('ROLE_USER','ROLE_MANAGER')")
+    public ResponseEntity<Page<MecanicienResponse>> searchMecaniciens(@RequestParam String keyword, @ParameterObject Pageable pageable) {
+        Page<MecanicienResponse> pageResult = mecanicienService.searchMecaniciens(keyword, pageable);
+        return ResponseEntity.ok(pageResult);
     }
 }
--- src/main/java/com/workshop/vehicle_service/mecanicien/dto/MecanicienResponse.java
+++ src/main/java/com/workshop/vehicle_service/mecanicien/dto/MecanicienResponse.java
@@ -1,6 +1,9 @@
 package com.workshop.vehicle_service.mecanicien.dto;
 
+import com.workshop.vehicle_service.intervention.entity.Intervention;
 import lombok.*;
+
+import java.util.List;
 
 @Data
 @Builder
@@ -12,4 +15,5 @@ public class MecanicienResponse {
     private String prenom;
     private String specialite;
     private boolean disponible;
+    private List<Intervention> interventions;
 }
